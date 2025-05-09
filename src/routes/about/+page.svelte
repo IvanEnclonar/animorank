3210a578b88340bca84166ca11953fdbe8afdcf9
ivanenclonar <paulivanenclonar@gmail.com>
2025-05-10 00:09:01 +0800

    feat: enhance about page with animated blobs and improved layout

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,13 +9,18 @@
 
 
 <div class="w-full flex flex-col items-center justify-center min-h-[120%] overflow-auto bg-[#121212]">
-    <div class="flex flex-col items-center justify-center w-full h-full p-4">
+    <div class="flex flex-col items-center justify-center w-full h-full p-4 z-10">
         <div class="bg-[#121D18] rounded-2xl p-2 px-4 flex w-fit text-forestGreen text-xs">DLSU's Official Coding Platform</div>
-        <div class="font-sans text-7xl font-bold mt-4 flex flex-col justify-center items-center lg:flex-row"><p class="text-center">Practice with&nbsp;</p><h1 class="text-accentColor">Animorank</h1></div>
+        <div class="font-sans text-7xl font-bold mt-4 flex flex-col justify-center items-center lg:flex-row"><p class="text-center">Practice with Animorank</div>
         <div class="mt-4 text-gray-400 text-xl block md:hidden text-center">Practice, and improve your programming skills with challenges</div>
         <div class="mt-4 text-gray-400 text-xl hidden md:block ">Practice, and improve your programming skills with challenges</div>
         <div class="text-gray-400 text-xl hidden md:block"> designed for DLSU students</div>
         <button class="btn btn-outline mt-5" onclick={() => {openLogin.open = true}}>Get started</button>
+    </div>
+    <div class="blob-cont">
+        <div class="yellow blob"></div>
+        <div class="red blob"></div>
+        <div class="green blob"></div>
     </div>
     <!-- <div class="w-full p-2 bg-[#040605]">
         <div class="w-full bg-[#000000] py-2 px-4 overflow-hidden">
@@ -24,7 +29,7 @@
             </div>
         </div>
     </div> -->
-    <footer class="bg-black py-8 px-8 w-full">
+    <footer class="bg-black py-8 px-8 w-full z-10">
         <div class="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
           <div>
             <div class="flex items-center mb-4">
@@ -51,3 +56,80 @@
         </div>
       </footer>
 </div>
+
+<style>
+    /* Blobs */
+
+.blob-cont {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    z-index: 5;
+    height: 900px;
+    width: 900px;
+    position: absolute;
+    right: 5em;
+}
+
+.blob {
+    border: 1px solid white;
+    border-radius: 200px;
+    filter: blur(60px);
+    opacity: 0;
+    animation-duration: 20s;
+    animation-timing-function: ease;
+    animation-iteration-count: infinite;
+}
+
+.yellow {
+    background-color: #025130;
+    position: absolute;
+    top: 200px;
+    right: 50px;
+    height: 300px;
+    width: 300px;
+    animation-name: yellow;
+}
+
+.green {
+    background-color: #114d32;
+    position: absolute;
+    top: 30px;
+    right: 50px;
+    height: 200px;
+    width: 250px;
+    animation-name: green;
+}
+
+.red {
+    background-color: #11472f;
+    position: absolute;
+    right: 0;
+    top: 300px;
+    height: 250px;
+    width: 200px;
+    animation-name: red;
+}
+
+@keyframes yellow {
+    0% {opacity: 0; top: 200px; left: 100px; transform: scale(1);}
+    30% {opacity: 1; top: 300px; left: 150px; transform: scale(1.2);}
+    60% {opacity: 1; top: 100px; left: 200px; transform: scale(1.3);}
+    100% {opacity: 0; top: 200px; left: 100px; transform: scale(1);}
+}
+
+@keyframes green {
+    0% {opacity: 0; top: 80px; right: -20px; transform: scale(1.2);}
+    30% {opacity: 1; top: 300px; right: -20px; transform: scale(1);}
+    60% {opacity: 1; top: 200px; right: 100px; transform: scale(1);}
+    100% {opacity: 0; top: 80px; right: -20px; transform: scale(1.2);}
+}
+
+@keyframes red {
+    0% {opacity: 0; top: 250px; right: 0px; transform: scale(1);}
+    30% {opacity: 1; top: 150px; right: 150px; transform: scale(1.4);}
+    60% {opacity: 1; top: 250px; right: 100px; transform: scale(1);}
+    100% {opacity: 0; top: 250px; right: 0px; transform: scale(1);}
+}
+</style>
